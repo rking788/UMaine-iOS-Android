96b50b36f5476ecd982acbd6deae3a1b3ba99598
jmonk@dream-crushed.com <jmonk@dream-crushed.com@ce22a5dd-5176-1b88-f200-a682eaf39866>
2011-02-18 20:58:21 +0000

    Working on classes to handle semesters, they currently can create local files and as classes are added they will store the data in the local filesystem and can load from there on next launch.

--- UMaine/src/org/umece/android/umaine/Course.java
+++ UMaine/src/org/umece/android/umaine/Course.java
@@ -9,65 +9,109 @@ public class Course {
 	private String times;
 	private String building;
 	private String roomnum;
+	
+	/**
+	 * Creates a course to be added to a semester.
+	 * 
+	 * @param cnum Course Number
+	 * @param ctitle Title
+	 * @param csession Session Number
+	 * @param cdesc Description
+	 * @param cdays Course Meeting Days
+	 * @param ctimes Course Times
+	 * @param cbuilding Course Location
+	 * @param croomnum Course Room
+	 * 
+	 * @author jmonk
+	 */
+	public Course(String cnum, String ctitle, String csession, String cdesc, 
+			String cdays, String ctimes, String cbuilding, String croomnum) {
+		coursenum = cnum;
+		title = ctitle;
+		sessionnum = csession;
+		description = cdesc;
+		days = cdays;
+		times = ctimes;
+		building = cbuilding;
+		roomnum = croomnum;
+	}
 
-	public void setCoursenum(String coursenum) {
-		this.coursenum = coursenum;
+	/**
+	 * Creates a course from the comma separated list found in each
+	 * semester's file. Throws exception if strings is not at least length
+	 * 8.
+	 * 
+	 * @param strings Course info in the following order: course_num, title,
+	 *					session_num, description, days, times, building, 
+	 *					room_num.
+	 *
+	 *@author jmonk
+	 */
+	public Course(String[] strings) {
+		for (String string : strings) {
+			string.replaceAll("\"", "");
+		}
+		
+		coursenum = strings[0];
+		title = strings[1];
+		sessionnum = strings[2];
+		description = strings[3];
+		days = strings[4];
+		times = strings[5];
+		building = strings[6];
+		roomnum = strings[7];
+	}
+	
+	/**
+	 * Returns all of the fields of the course separated by |.
+	 * They are listed in the following order: course_num, title,
+	 *					session_num, description, days, times, building, 
+	 *					room_num.
+	 *
+	 * @author jmonk
+	 */
+	@Override
+	public String toString() {
+		String return_val;
+		
+		return_val = "\"" + coursenum + "\",\"";
+		return_val = (return_val + title + "\",\"");
+		return_val = (return_val + sessionnum + "\",\"");
+		return_val = (return_val + description + "\",\"");
+		return_val = (return_val + days + "\",\"");
+		return_val = (return_val + times + "\",\"");
+		return_val = (return_val + building + "\",\"");
+		return_val = (return_val + roomnum + "\"");
+		
+		return return_val;
 	}
 	
 	public String getCoursenum() {
 		return coursenum;
 	}
 	
-	public void setTitle(String title) {
-		this.title = title;
-	}
-	
 	public String getTitle() {
 		return title;
-	}
-	
-	public void setSessionnum(String sessionnum) {
-		this.sessionnum = sessionnum;
 	}
 	
 	public String getSessionnum() {
 		return sessionnum;
 	}
 	
-	public void setDescription(String description) {
-		this.description = description;
-	}
-	
 	public String getDescription() {
 		return description;
-	}
-	
-	public void setDays(String days) {
-		this.days = days;
 	}
 	
 	public String getDays() {
 		return days;
 	}
 	
-	public void setTimes(String times) {
-		this.times = times;
-	}
-	
 	public String getTimes() {
 		return times;
 	}
 	
-	public void setBuilding(String building) {
-		this.building = building;
-	}
-	
 	public String getBuilding() {
 		return building;
-	}
-	
-	public void setRoomnum(String roomnum) {
-		this.roomnum = roomnum;
 	}
 	
 	public String getRoomnum() {
---
+++ UMaine/src/org/umece/android/umaine/Semester.java
@@ -0,0 +1,157 @@
+package org.umece.android.umaine;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import android.app.Activity;
+import android.content.Context;
+
+public class Semester {
+	private String campus;
+	private String year;
+	private String season;
+	private String department;
+	
+	private List<Course> courses;
+	private Activity act;
+	private String file;
+
+	/**
+	 * Creates an empty semester and the associated file
+	 * for storing classes.
+	 * 
+	 * @param  file  The file to be created to hold semester data.
+	 * @param  act  The activity that will be used to open the semester file.
+	 * 
+	 * @author jmonk
+	 */
+	public Semester(String file, Activity act, String campus, String year, String season, String department) throws IOException {
+		FileOutputStream fos = act.openFileOutput(file, Context.MODE_PRIVATE);
+		
+		String write_string;
+		write_string = campus + "," + year + "," + season + "," + department;
+		
+		fos.write(write_string.getBytes());
+		
+		fos.close();
+		
+		this.file = file;
+		this.act = act;
+		this.campus = campus;
+		this.department = department;
+		this.season = season;
+		this.year = year;
+		courses = new ArrayList<Course>();
+	}
+
+	/**
+	 * Creates a semester loading data from an existing file.
+	 * The file must exist already
+	 * 
+	 * @param  file  The full path to file that will be opened.
+	 * @param  act  The activity that will be used to open the file, reused upon adding of classes.
+	 * 
+	 * @author jmonk
+	 */
+	public Semester(String file, Activity act) throws IOException {
+		this.setCourses(new ArrayList<Course>());
+		this.file = file;
+		this.act = act;
+		
+		FileInputStream fis = act.openFileInput(file);
+		byte[] bytes = new byte[200];
+		String read = new String();
+		int len;
+		while ((len = fis.read(bytes)) != -1) {
+			read = (read + new String(bytes, 0, len));
+		}
+		
+		String pieces[] = read.split(";");
+		String line[] = pieces[0].split(",");
+		campus = line[0];
+		year = line[1];
+		season = line[2];
+		department = line[3];
+		
+		int i;
+		for (i = 1; i < pieces.length; i++) {
+			courses.add(new Course(pieces[i].split("\",\"")));
+		}
+		
+		fis.close();
+	}
+
+	public String getFile() {
+		return file;
+	}
+
+	public void setCampus(String campus) {
+		this.campus = campus;
+	}
+
+	public String getCampus() {
+		return campus;
+	}
+
+	public void setYear(String year) {
+		this.year = year;
+	}
+
+	public String getYear() {
+		return year;
+	}
+
+	public void setSeason(String season) {
+		this.season = season;
+	}
+
+	public String getSeason() {
+		return season;
+	}
+
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+
+	public String getDepartment() {
+		return department;
+	}
+
+	public void setCourses(List<Course> courses) {
+		this.courses = courses;
+	}
+	
+	public int size() {
+		return courses.size();
+	}
+
+	public Course getCourse(int i) {
+		return courses.get(i);
+	}
+	
+	/**
+	 * This adds a course to semester's file then to its local list of courses.
+	 * If an exception is thrown then the course is not added to the semester.
+	 * 
+	 * @param new_course Course to be added to the semester, cannot be modified once added.
+	 * 
+	 * @throws IOException IOExceptions are thrown if the file cannot be opened or cannot be appended to.
+	 * 
+	 * @author jmonk
+	 */
+	public void addCourse(Course new_course) throws IOException {
+		FileOutputStream fos = act.openFileOutput(file, Context.MODE_APPEND);
+		
+		fos.write((";" + new_course.toString()).getBytes());
+		
+		fos.close();
+		
+		courses.add(new_course);
+	}
+}
