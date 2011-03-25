df3d19a277581aa7dcf39e6b326d35889ac6c015
zhuyifeng@gmail.com <zhuyifeng@gmail.com@ce22a5dd-5176-1b88-f200-a682eaf39866>
2011-03-25 14:33:05 +0000

    Add sport

---
+++ UMaine/src/org/umece/android/umaine/SportsDisplay.java
@@ -0,0 +1,134 @@
+package org.umece.android.umaine;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class SportsDisplay extends Activity {
+	public static String results = "bbbbbbbbbbbbb";
+	
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.sportsdisplay);
+
+		TextView textView = (TextView) findViewById(R.id.text_view);
+		textView.setText(showResults());
+		
+	}
+	
+	/**
+	 * Date\tEvent \t\t\tLocation\t\t\tTime Band
+	 * 
+	 * @param option
+	 */
+	public void input(String option) {
+		if (option == "allSch") {
+			// modifyString("allSch");
+			results = "10/01 \tPresident's Event \t\tCCA\t\t\t6:00PM  Full\n"
+					+ "10/03 \tHockey \t\t\t\tAlfond\t\t\t4:00PM\tA\n"
+					+ "10/08 \tHockey \t\t\t\tAlfond\t\t\t7:00PM\tB\n"
+					+ "10/09 \tHockey \t\t\t\tAlfond\t\t\t7:00PM\tA\n"
+					+ "10/22 \tHockey \t\t\t\tAlfond\t\t\t7:00PM\tB\n"
+					+ "10/23 \tHockey \t\t\t\tAlfond\t\t\t7:00PM\tA\n"
+					+ "11/12 \tHockey \t\t\t\tAlfond\t\t\t7:00PM\tB\n"
+					+ "11/13 \tWomens Basketball \tAlfond\t\t\t12:00PM\tA\n"
+					+ "11/13 \tHockey \t\t\t\tAlfond\t\t\t7:00PM\tB\n"
+					+ "11/18 \tMens Basketball \t\tAlfond\t\t\tTBA\t\tB\n"
+					+ "11/23 \tWomens Basketball \tAlfond\t\t\t7:00PM\tA\n"
+					+ "11/27 \tWomens Basketball \tAlfond\t\t\tTBA\t\tB\n"
+					+ "12/04 \tMens Basketball \t\tAlfond\t\t\t2:00PM\tA\n"
+					+ "12/06 \tMens Basketball \t\tAlfond\t\t\t7:30PM\tB\n"
+					+ "12/10 \tHockey \t\t\t\tAlfond\t\t\t7:00PM\tA\n"
+					+ "12/12 \tMens Basketball \t\tPit\t\t\t\t12:00PM\tA\n"
+					+ "12/12 \tHockey \t\t\t\tAlfond\t\t\t4:00PM\tB\n"
+					+ "12/19 \tMens Basketball \t\tAlfond\t\t\t2:00PM\tB\n"
+					+ "01/02 \tMens Basketball \t\tAlfond\t\t\tTBA\t\tA\n"
+					+ "01/05 \tMens Basketball \t\tAlfond\t\t\t7:00PM\tB\n"
+					+ "01/09 \tWomens Basketball \tAlfond\t\t\t12:00PM\tA\n"
+					+ "01/12 \tWomens Basketball \tAlfond\t\t\t7:30PM\tB\n"
+					+ "01/14 \tHockey \t\t\t\tAlfond \t\t\t7:00PM\tB\n"
+					+ "01/15 \tWomens Basketball \tAlfond\t\t\t1:00PM\tA\n"
+					+ "01/15 \tMens Basketball \t\tAlfond\t\t\t3:00PM\tB\n"
+					+ "01/16 \tHockey \t\t\t\tAlfond \t\t\t4:00PM\tA\n"
+					+ "01/22 \tMens Basketball \t\tAlfond(Band Day)7:00PM  Full\n"
+					+ "01/23 \tWomens Basketball \tAlfond\t\t\t1:00PM\tB\n"
+					+ "01/25 \tMens Basketball \t\tAlfond\t\t\t7:00PM\tA\n"
+					+ "01/26 \tWomens Basketball \tAlfond\t\t\t7:00PM\tB\n"
+					+ "01/28 \tHockey \t\t\t\tAlfond \t\t\t7:00PM\tA\n"
+					+ "01/29 \tHockey \t\t\t\tAlfond \t\t\t7:00PM\tB\n"
+					+ "02/01 \tWomens Basketball \tAlfond\t\t\t7:00PM\tA\n"
+					+ "02/06 \tMens Basketball \t\tAlfond\t\t\t2:00PM\tB\n"
+					+ "02/08 \tWomens Basketball \tAlfond\t\t\t7:00PM\tAn\n"
+					+ "02/11 \tHockey \t\t\t\tAlfond \t\t\t7:00PM\tB\n"
+					+ "02/12 \tHockey \t\t\t\tAlfond \t\t\t7:00PM\tA\n"
+					+ "02/16 \tMens Basketball \t\tAlfond\t\t\t7:30PM\tB\n"
+					+ "02/20 \tWomens Basketball \tAlfond\t\t\t1:00PM\tA\n"
+					+ "02/21 \tOpen House \t\t\tCCA \t\t\t8:30AM  Full\n"
+					+ "02/25 \tHockey \t\t\t\tAlfond \t\t\t7:00PM\tB\n"
+					+ "02/26 \tHockey \t\t\t\tAlfond \t\t\t7:00PM\tA\n"
+					+ "02/27 \tMens Basketball \t\tAlfond\t\t\t2:00PM\tB\n"
+					+ "03/11-03/13\n\t\tHockey East Quarter Finals TBD\t\tTBD\tPlayoff\n"
+					+ "03/18-03/19\n\t\tHockey East Finals\t\tBoston \t\tTBD\tPlayoff\n"
+					+ "03/25-03/27\n\t\tHockey NCAA Regionals	TBD\t\tTBD\tPlayoff\n"
+					+ "04/07-04/09\n\t\tHockey Frozen Four\tSt. Paul, MN \tTBD\tPlayoff";
+		} else if (option == "hockeySch") {
+			// modifyString("hockeySch");
+			results = "10/03 \tHockey \t\t\t\tAlfond\t\t\t4:00PM\tA\n"
+					+ "10/08 \tHockey \t\t\t\tAlfond\t\t\t7:00PM\tB\n"
+					+ "10/09 \tHockey \t\t\t\tAlfond\t\t\t7:00PM\tA\n"
+					+ "10/22 \tHockey \t\t\t\tAlfond\t\t\t7:00PM\tB\n"
+					+ "10/23 \tHockey \t\t\t\tAlfond\t\t\t7:00PM\tA\n"
+					+ "11/12 \tHockey \t\t\t\tAlfond\t\t\t7:00PM\tB\n"
+					+ "11/13 \tHockey \t\t\t\tAlfond\t\t\t7:00PM\tB\n"
+					+ "12/10 \tHockey \t\t\t\tAlfond\t\t\t7:00PM\tA\n"
+					+ "12/12 \tHockey \t\t\t\tAlfond\t\t\t4:00PM\tB\n"
+					+ "01/14 \tHockey \t\t\t\tAlfond \t\t\t7:00PM\tB\n"
+					+ "01/16 \tHockey \t\t\t\tAlfond \t\t\t4:00PM\tA\n"
+					+ "01/28 \tHockey \t\t\t\tAlfond \t\t\t7:00PM\tA\n"
+					+ "01/29 \tHockey \t\t\t\tAlfond \t\t\t7:00PM\tB\n"
+					+ "02/11 \tHockey \t\t\t\tAlfond \t\t\t7:00PM\tB\n"
+					+ "02/12 \tHockey \t\t\t\tAlfond \t\t\t7:00PM\tA\n"
+					+ "02/25 \tHockey \t\t\t\tAlfond \t\t\t7:00PM\tB\n"
+					+ "02/26 \tHockey \t\t\t\tAlfond \t\t\t7:00PM\tA\n"
+					+ "03/11-03/13\n\t\tHockey East Quarter Finals TBD\t\tTBD\tPlayoff\n"
+					+ "03/18-03/19\n\t\tHockey East Finals\t\tBoston \t\tTBD\tPlayoff\n"
+					+ "03/25-03/27\n\t\tHockey NCAA Regionals	TBD\t\tTBD\tPlayoff\n"
+					+ "04/07-04/09\n\t\tHockey Frozen Four\tSt. Paul, MN \tTBD\tPlayoff";
+		} else if (option == "basketballSch") {
+			results = "11/13 \tWomens Basketball \tAlfond\t\t\t12:00PM\tA\n"
+					+ "11/18 \tMens Basketball \t\tAlfond\t\t\tTBA\t\tB\n"
+					+ "11/23 \tWomens Basketball \tAlfond\t\t\t7:00PM\tA\n"
+					+ "11/27 \tWomens Basketball \tAlfond\t\t\tTBA\t\tB\n"
+					+ "12/04 \tMens Basketball \t\tAlfond\t\t\t2:00PM\tA\n"
+					+ "12/06 \tMens Basketball \t\tAlfond\t\t\t7:30PM\tB\n"
+					+ "12/12 \tMens Basketball \t\tPit\t\t\t\t12:00PM\tA\n"
+					+ "12/19 \tMens Basketball \t\tAlfond\t\t\t2:00PM\tB\n"
+					+ "01/02 \tMens Basketball \t\tAlfond\t\t\tTBA\t\tA\n"
+					+ "01/05 \tMens Basketball \t\tAlfond\t\t\t7:00PM\tB\n"
+					+ "01/09 \tWomens Basketball \tAlfond\t\t\t12:00PM\tA\n"
+					+ "01/12 \tWomens Basketball \tAlfond\t\t\t7:30PM\tB\n"
+					+ "01/15 \tWomens Basketball \tAlfond\t\t\t1:00PM\tA\n"
+					+ "01/15 \tMens Basketball \t\tAlfond\t\t\t3:00PM\tB\n"
+					+ "01/22 \tMens Basketball \t\tAlfond(Band Day)7:00PM  Full\n"
+					+ "01/23 \tWomens Basketball \tAlfond\t\t\t1:00PM\tB\n"
+					+ "01/25 \tMens Basketball \t\tAlfond\t\t\t7:00PM\tA\n"
+					+ "01/26 \tWomens Basketball \tAlfond\t\t\t7:00PM\tB\n"
+					+ "02/01 \tWomens Basketball \tAlfond\t\t\t7:00PM\tA\n"
+					+ "02/06 \tMens Basketball \t\tAlfond\t\t\t2:00PM\tB\n"
+					+ "02/08 \tWomens Basketball \tAlfond\t\t\t7:00PM\tAn\n"
+					+ "02/16 \tMens Basketball \t\tAlfond\t\t\t7:30PM\tB\n"
+					+ "02/20 \tWomens Basketball \tAlfond\t\t\t1:00PM\tA\n"
+					+ "02/27 \tMens Basketball \t\tAlfond\t\t\t2:00PM\tB\n";
+		} else if (option == "othersSch") {
+
+			results = "10/01 \tPresident's Event \t\tCCA\t\t\t6:00PM  Full\n"
+					+ "02/21 \tOpen House \t\t\tCCA \t\t\t8:30AM  Full\n";
+		}
+	}
+
+	public static String showResults() {
+
+		return results;
+	}
+}
--- UMaine/src/org/umece/android/umaine/UMSports.java
+++ UMaine/src/org/umece/android/umaine/UMSports.java
@@ -1,17 +1,32 @@
 package org.umece.android.umaine;
 
-
+import android.app.ActivityGroup;
 import android.app.TabActivity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+
 import android.widget.TabHost;
+import android.widget.TextView;
+import android.widget.TabHost.TabSpec;
 
-public class UMSports extends TabActivity {
+public class UMSports extends ActivityGroup {
 
+	public static TabHost tab_host;
+	public static final int update = Menu.FIRST;
+	public static final int help = Menu.FIRST+1;
+	
+	public String all_results = "bbbbbbbb";
+	
+	public SportsDisplay sd = new SportsDisplay();
+	
+	
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		TabHost tabHost = getTabHost();
-		LayoutInflater.from(this).inflate(R.layout.tab_sports,
+/*		TabHost tabHost = getTabHost();
+		LayoutInflater.from(this).inflate(R.layout.sports,
 				tabHost.getTabContentView(), true);
 		tabHost.addTab(tabHost.newTabSpec("tab1").setIndicator("All")
 				.setContent(R.id.tab_view1));
@@ -20,7 +35,63 @@ public class UMSports extends TabActivity {
 		tabHost.addTab(tabHost.newTabSpec("tab3").setIndicator("Basketball")
 				.setContent(R.id.tab_view3));
 		tabHost.addTab(tabHost.newTabSpec("tab4").setIndicator("Other")
-				.setContent(R.id.tab_view4));
-	}
+				.setContent(R.id.tab_view4));*/
+		
+		setContentView(R.layout.sprotsbottomtab);
 
+		//TextView textView = (TextView) findViewById(R.id.text_view);
+		//textView.setText(showResults());
+		
+        tab_host = (TabHost) findViewById(R.id.edit_item_tab_host);
+        tab_host.setup(this.getLocalActivityManager());
+
+        sd.input("allSch");
+        TabSpec ts1 = tab_host.newTabSpec("tab_all_sch"); 
+        ts1.setIndicator("All Sch");
+        ts1.setContent(new Intent(this, SportsDisplay.class)); 
+//        sd.input("allSch");
+        tab_host.addTab(ts1);
+
+        sd.input("hockeySch");
+        TabSpec ts2 = tab_host.newTabSpec("tab_hockey_sch");       
+        ts2.setIndicator("Hockey"); 
+        ts2.setContent(new Intent(this, SportsDisplay.class)); 
+//        sd.input("hockeySch"); 
+        tab_host.addTab(ts2);
+
+        sd.input("basketballSch");
+        TabSpec ts3 = tab_host.newTabSpec("tab_basketball_sch");        
+        ts3.setIndicator("Basketball"); 
+        ts3.setContent(new Intent(this, SportsDisplay.class)); 
+//        sd.input("basketballSch");
+        tab_host.addTab(ts3);
+        
+        sd.input("othersSch");
+        TabSpec ts4 = tab_host.newTabSpec("tab_others_sch"); 
+        ts4.setIndicator("Others");   
+        ts4.setContent(new Intent(this, SportsDisplay.class)); 
+//      sd.input("othersSch");
+        tab_host.addTab(ts4);
+
+        tab_host.setCurrentTab(0);
+	}
+	
+	public boolean onCreateOptionsMenu(Menu menu) {
+		menu.add(0,update,0,"Update");
+//		menu.add(0,help,1,"Help");
+		return super.onCreateOptionsMenu(menu);  
+		
+	}
+	public boolean onOptionsItemSelected(MenuItem item) {  
+        //TextView tv = (TextView)findViewById(R.id.tv01);  
+        switch(item.getItemId()){  
+            case update:
+            	// here is the function of update
+            	break;  
+/*            case help:
+            	// here is the funcion of help
+            	break; */ 
+        }  
+        return super.onOptionsItemSelected(item);  
+    }  
 }
