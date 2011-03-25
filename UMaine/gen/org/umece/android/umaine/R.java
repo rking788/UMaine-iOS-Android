7767bb55df4c4a4c7edc750f20981de17fdcc035
731jerry@gmail.com <731jerry@gmail.com@ce22a5dd-5176-1b88-f200-a682eaf39866>
2011-03-25 15:53:51 +0000

    modify the icons of project

--- UMaine/gen/org/umece/android/umaine/R.java
+++ UMaine/gen/org/umece/android/umaine/R.java
@@ -8,34 +8,87 @@
 package org.umece.android.umaine;
 
 public final class R {
+    public static final class array {
+        public static final int departments=0x7f050000;
+    }
     public static final class attr {
     }
     public static final class drawable {
-        public static final int commutermarker=0x7f020000;
-        public static final int icon=0x7f020001;
-        public static final int residentmarker=0x7f020002;
-        public static final int staffmarker=0x7f020003;
+        public static final int blackbear=0x7f020000;
+        public static final int blackbearold=0x7f020001;
+        public static final int commutermarker=0x7f020002;
+        public static final int icon=0x7f020003;
+        public static final int mapbutton=0x7f020004;
+        public static final int residentmarker=0x7f020005;
+        public static final int schedulebackground=0x7f020006;
+        public static final int staffmarker=0x7f020007;
     }
     public static final class id {
-        public static final int buildings=0x7f060005;
-        public static final int coursesbtn=0x7f060001;
-        public static final int courseslabel=0x7f060000;
-        public static final int lots=0x7f060004;
-        public static final int mapview=0x7f060003;
-        public static final int parkingbtn=0x7f060002;
+        public static final int ScrollView=0x7f070013;
+        public static final int addcourse=0x7f07001a;
+        public static final int addcourseroot=0x7f070000;
+        public static final int buildings=0x7f070020;
+        public static final int cancel=0x7f07001e;
+        public static final int cdetail_title_tv=0x7f070007;
+        public static final int cdetails_delete_btn=0x7f07000a;
+        public static final int cdetails_map_btn=0x7f070009;
+        public static final int cdetails_tv=0x7f070008;
+        public static final int change_semester=0x7f07001b;
+        public static final int courselist_directions=0x7f07000c;
+        public static final int coursenum_tv=0x7f070003;
+        public static final int coursenumspin=0x7f070004;
+        public static final int courses_semester=0x7f07000d;
+        public static final int coursesbtn=0x7f07000f;
+        public static final int courseslist=0x7f07000e;
+        public static final int delete=0x7f07001d;
+        public static final int delete_semester=0x7f07001c;
+        public static final int depart_ac=0x7f070002;
+        public static final int depart_tv=0x7f070001;
+        public static final int edit_item_tab_host=0x7f070019;
+        public static final int lots=0x7f07001f;
+        public static final int mapview=0x7f070012;
+        public static final int parkingbtn=0x7f070010;
+        public static final int scheduledraw=0x7f07000b;
+        public static final int sectionnum_tv=0x7f070005;
+        public static final int sectionspin=0x7f070006;
+        public static final int sportsbtn=0x7f070011;
+        public static final int tab_view1=0x7f070014;
+        public static final int tab_view2=0x7f070015;
+        public static final int tab_view3=0x7f070016;
+        public static final int tab_view4=0x7f070017;
+        public static final int text_view=0x7f070018;
     }
     public static final class layout {
-        public static final int courses=0x7f030000;
-        public static final int main=0x7f030001;
-        public static final int map=0x7f030002;
+        public static final int addcoursedialog=0x7f030000;
+        public static final int coursedetails=0x7f030001;
+        public static final int courses=0x7f030002;
+        public static final int list_item=0x7f030003;
+        public static final int main=0x7f030004;
+        public static final int map=0x7f030005;
+        public static final int sports=0x7f030006;
+        public static final int sportsdisplay=0x7f030007;
+        public static final int sprotsbottomtab=0x7f030008;
+        public static final int tab_sports=0x7f030009;
     }
     public static final class menu {
-        public static final int map_menu=0x7f050000;
+        public static final int courses_menu=0x7f060000;
+        public static final int delete_course_conmenu=0x7f060001;
+        public static final int map_menu=0x7f060002;
     }
     public static final class string {
+        public static final int allSch_button_title=0x7f040008;
         public static final int app_name=0x7f040001;
-        public static final int buildings=0x7f040002;
+        public static final int basketballSch_button_title=0x7f04000a;
+        public static final int courselist_directions=0x7f040005;
+        public static final int coursenum_prompt=0x7f040003;
+        public static final int depart_prompt=0x7f040002;
         public static final int hello=0x7f040000;
-        public static final int lots=0x7f040003;
+        public static final int hello_sports=0x7f040007;
+        public static final int hockeySch_button_title=0x7f040009;
+        public static final int othersSch_button_title=0x7f04000b;
+        public static final int search_title=0x7f04000d;
+        public static final int section_prompt=0x7f040004;
+        public static final int sports_title=0x7f04000c;
+        public static final int start_title=0x7f040006;
     }
 }
