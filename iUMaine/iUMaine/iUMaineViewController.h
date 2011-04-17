ac8b0b2a6330956c06228247544703e56262ff7a
Rking188@gmail.com <Rking188@gmail.com@ce22a5dd-5176-1b88-f200-a682eaf39866>
2011-04-17 04:58:53 +0000

    Initial commit of the iOS project directory. Used the View Based project template.

---
+++ iUMaine/iUMaine/iUMaineViewController.h
@@ -0,0 +1,15 @@
+//
+//  iUMaineViewController.h
+//  iUMaine
+//
+//  Created by Teacher on 4/17/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface iUMaineViewController : UIViewController {
+    
+}
+
+@end
