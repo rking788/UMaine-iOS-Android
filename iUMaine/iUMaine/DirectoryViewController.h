e1d7ba186121a9ae9c93b23e036aa06b9548ee38
zhuyifeng@gmail.com <zhuyifeng@gmail.com@ce22a5dd-5176-1b88-f200-a682eaf39866>
2011-04-26 14:52:42 +0000

    Still has bug

--- iUMaine/iUMaine/DirectoryViewController.h
+++ iUMaine/iUMaine/DirectoryViewController.h
@@ -1,31 +1,21 @@
-//
-//  DirectoryViewController.h
-//  iUMaine
-//
-//  Created by RKing on 4/17/11.
-//  Copyright 2011 UMaineIRL. All rights reserved.
-//
 
 #import <UIKit/UIKit.h>
 
 
-@interface DirectoryViewController : UITableViewController <UISearchDisplayDelegate, UISearchBarDelegate>
+@interface DirectoryViewController : UIViewController <UITableViewDataSource, UITableViewDelegate, UISearchDisplayDelegate, UISearchBarDelegate>
 {
-	NSArray			*listContent;			// The master content.
-	NSMutableArray	*filteredListContent;	// The content filtered as a result of a search.
-    //UILocalizedIndexedCollation *collation;
+	UITableView     *mainTableView;
 	
-	// The saved state of the search UI if a memory warning removed the view.
-    NSString		*savedSearchTerm;
-    NSInteger		savedScopeButtonIndex;
-    BOOL			searchWasActive;
+	NSMutableArray  *contentsList;
+	NSMutableArray  *searchResults;
+	NSString        *savedSearchTerm;
 }
 
-@property (nonatomic, retain) NSArray *listContent;
-@property (nonatomic, retain) NSMutableArray *filteredListContent;
+@property (nonatomic, retain) IBOutlet UITableView *mainTableView;
+@property (nonatomic, retain) NSMutableArray *contentsList;
+@property (nonatomic, retain) NSMutableArray *searchResults;
+@property (nonatomic, copy)   NSString *savedSearchTerm;
 
-@property (nonatomic, copy) NSString *savedSearchTerm;
-@property (nonatomic) NSInteger savedScopeButtonIndex;
-@property (nonatomic) BOOL searchWasActive;
+- (void)handleSearchForTerm:(NSString *)searchTerm;
 
 @end
--- iUMaine/iUMaine/DirectoryViewController.m
+++ iUMaine/iUMaine/DirectoryViewController.m
@@ -1,213 +1,203 @@
-//
-//  DirectoryViewController.m
-//  iUMaine
-//
-//  Created by RKing on 4/17/11.
-//  Copyright 2011 UMaineIRL. All rights reserved.
-//
+
 
 #import "DirectoryViewController.h"
-#import "Person.h"
 
 @implementation DirectoryViewController
-@synthesize listContent, filteredListContent, savedSearchTerm, savedScopeButtonIndex, searchWasActive;
 
-
-- (void)viewDidLoad
-{
-    
-    
-	self.title = @"Directory";
-	
-    // Create the master list for the main view controller.
-	 self.listContent = [[NSArray alloc] initWithObjects:
-            [Person personWithType:@"Device" firstName:@"iPhone" lastName:@"iPhone"],
-            [Person personWithType:@"Device" firstName:@"iPod" lastName:@"iPhone"],
-            [Person personWithType:@"Device" firstName:@"iPod touch" lastName:@"iPhone"],
-            [Person personWithType:@"Desktop" firstName:@"iMac" lastName:@"iPhone"],
-            [Person personWithType:@"Desktop" firstName:@"Mac Pro" lastName:@"iPhone"],
-            [Person personWithType:@"Portable" firstName:@"iBook" lastName:@"iPhone"],
-            [Person personWithType:@"Portable" firstName:@"MacBook" lastName:@"iPhone"],
-            [Person personWithType:@"Portable" firstName:@"MacBook Pro" lastName:@"iPhone"],
-            [Person personWithType:@"Portable" firstName:@"PowerBook" lastName:@"iPhone"], nil];
-    
-	// create a filtered list that will contain products for the search results table.
-	self.filteredListContent = [NSMutableArray arrayWithCapacity:[self.listContent count]];
-	
-	// restore search settings if they were saved in didReceiveMemoryWarning.
-    if (self.savedSearchTerm)
-	{
-        [self.searchDisplayController setActive:self.searchWasActive];
-        [self.searchDisplayController.searchBar setSelectedScopeButtonIndex:self.savedScopeButtonIndex];
-        [self.searchDisplayController.searchBar setText:savedSearchTerm];
-        
-        self.savedSearchTerm = nil;
-    }
-	
-	[self.tableView reloadData];
-	self.tableView.scrollEnabled = YES;
-    
-    [super viewDidLoad];
-   
-}
-
-
-- (void)viewDidDisappear:(BOOL)animated
-{
-    // save the state of the search UI so that it can be restored if the view is re-created
-    self.searchWasActive = [self.searchDisplayController isActive];
-    self.savedSearchTerm = [self.searchDisplayController.searchBar text];
-    self.savedScopeButtonIndex = [self.searchDisplayController.searchBar selectedScopeButtonIndex];
-}
-
-
-- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
-{
-    // Return YES for supported orientations
-    return (interfaceOrientation == UIInterfaceOrientationPortrait);
-}
-
-
-- (void)didReceiveMemoryWarning
-{
-    // Releases the view if it doesn't have a superview.
-    [super didReceiveMemoryWarning];
-    
-    // Release any cached data, images, etc. that aren't in use.
-}
-
-
-- (void)viewDidUnload
-{
-    self.filteredListContent = nil;
-    [super viewDidUnload];
-
-    // Release any retained subviews of the main view.
-    // e.g. self.myOutlet = nil;
-}
-
+@synthesize mainTableView;
+@synthesize contentsList;
+@synthesize searchResults;
+@synthesize savedSearchTerm;
 
 - (void)dealloc
 {
-    [listContent release];
-	[filteredListContent release];
-    [super dealloc];
-}
-
-- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
-{
-	/*
-	 If the requesting table view is the search display controller's table view, return the count of
-     the filtered list, otherwise return the count of the main list.
-	 */
-	if (tableView == self.searchDisplayController.searchResultsTableView)
-	{
-        return [self.filteredListContent count];
-    }
-	else
-	{
-        return [self.listContent count];
-    }
-}
-
-
-- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
-{
-	static NSString *kCellID = @"cellID";
+	NSLog(@">>> Entering %s <<<", __PRETTY_FUNCTION__);
 	
-	UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:kCellID];
-	if (cell == nil)
+	[contentsList release], contentsList = nil;
+	[searchResults release], searchResults = nil;
+	[savedSearchTerm release], savedSearchTerm = nil;
+	[mainTableView release], mainTableView = nil;
+    [super dealloc];
+	
+	NSLog(@"<<< Leaving %s >>>", __PRETTY_FUNCTION__);
+}
+
+- (void)viewDidUnload
+{
+	NSLog(@">>> Entering %s <<<", __PRETTY_FUNCTION__);
+	
+	
+	// Save the state of the search UI so that it can be restored if the view is re-created.
+	[self setSavedSearchTerm:[[[self searchDisplayController] searchBar] text]];
+	
+	[self setSearchResults:nil];
+	
+    [super viewDidUnload];
+    
+	NSLog(@"<<< Leaving %s >>>", __PRETTY_FUNCTION__);
+}
+
+- (void)viewDidLoad
+{
+	NSLog(@">>> Entering %s <<<", __PRETTY_FUNCTION__);
+	
+    
+	NSMutableArray *array = [[NSMutableArray alloc] initWithObjects:@"Red", @"Blue", @"Green", @"Black", @"Purple", nil];
+	[self setContentsList:array];
+	[array release], array = nil;
+	
+	// Restore search term
+	if ([self savedSearchTerm])
 	{
-		cell = [[[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:kCellID] autorelease];
-		cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+        [[[self searchDisplayController] searchBar] setText:[self savedSearchTerm]];
+    }
+	
+    [super viewDidLoad];
+    
+	NSLog(@"<<< Leaving %s >>>", __PRETTY_FUNCTION__);
+}
+
+- (void)viewWillAppear:(BOOL)animated
+{
+	NSLog(@">>> Entering %s <<<", __PRETTY_FUNCTION__);
+	
+    [super viewWillAppear:animated];
+	
+	[[self mainTableView] reloadData];
+	
+	NSLog(@"<<< Leaving %s >>>", __PRETTY_FUNCTION__);
+}
+
+/*
+// Override to allow orientations other than the default portrait orientation.
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
+    // Return YES for supported orientations
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+*/
+
+- (void)didReceiveMemoryWarning {
+	// Releases the view if it doesn't have a superview.
+    [super didReceiveMemoryWarning];
+	
+	// Release any cached data, images, etc that aren't in use.
+}
+
+- (void)handleSearchForTerm:(NSString *)searchTerm
+{
+	NSLog(@">>> Entering %s <<<", __PRETTY_FUNCTION__);
+	
+	[self setSavedSearchTerm:searchTerm];
+	
+	if ([self searchResults] == nil)
+	{
+		NSMutableArray *array = [[NSMutableArray alloc] init];
+		[self setSearchResults:array];
+		[array release], array = nil;
 	}
 	
-	/*
-	 If the requesting table view is the search display controller's table view, configure the cell using the filtered content, otherwise use the main list.
-	 */
-	Person *person = nil;
-	if (tableView == self.searchDisplayController.searchResultsTableView)
-	{
-        person = [self.filteredListContent objectAtIndex:indexPath.row];
-    }
-	else
-	{
-        person = [self.listContent objectAtIndex:indexPath.row];
-    }
+	[[self searchResults] removeAllObjects];
 	
-	cell.textLabel.text = person.firstName;
+	if ([[self savedSearchTerm] length] != 0)
+	{
+		for (NSString *currentString in [self contentsList])
+		{
+			if ([currentString rangeOfString:searchTerm options:NSCaseInsensitiveSearch].location != NSNotFound)
+			{
+				[[self searchResults] addObject:currentString];
+			}
+		}
+	}
+	
+	NSLog(@"<<< Leaving %s >>>", __PRETTY_FUNCTION__);
+}
+
+#pragma mark -
+#pragma mark UITableViewDataSource Methods
+
+- (NSInteger)tableView:(UITableView *)tableView
+ numberOfRowsInSection:(NSInteger)section
+{
+	NSLog(@">>> Entering %s <<<", __PRETTY_FUNCTION__);
+	
+	NSInteger rows;
+	
+	if (tableView == [[self searchDisplayController] searchResultsTableView])
+		rows = [[self searchResults] count];
+	else
+		rows = [[self contentsList] count];
+	
+	NSLog(@"rows is: %d", rows);
+	NSLog(@"<<< Leaving %s >>>", __PRETTY_FUNCTION__);
+	return rows;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView
+		 cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+	NSLog(@">>> Entering %s <<<", __PRETTY_FUNCTION__);
+	
+	NSInteger row = [indexPath row];
+	NSString *contentForThisRow = nil;
+	
+	if (tableView == [[self searchDisplayController] searchResultsTableView])
+		contentForThisRow = [[self searchResults] objectAtIndex:row];
+	else
+		contentForThisRow = [[self contentsList] objectAtIndex:row];
+	
+	static NSString *CellIdentifier = @"CellIdentifier";
+	
+	UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+	if (cell == nil)
+	{
+		cell = [[[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier] autorelease];
+		// Do anything that should be the same on EACH cell here.  Fonts, colors, etc.
+	}
+	
+	// Do anything that COULD be different on each cell here.  Text, images, etc.
+	[[cell textLabel] setText:contentForThisRow];
+	
+	NSLog(@"<<< Leaving %s >>>", __PRETTY_FUNCTION__);
 	return cell;
 }
 
+#pragma mark -
+#pragma mark UITableViewDelegate Methods
 
-- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+- (void)tableView:(UITableView *)tableView
+didSelectRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    UIViewController *detailsViewController = [[UIViewController alloc] init];
-    
-	/*
-	 If the requesting table view is the search display controller's table view, configure the next view controller using the filtered content, otherwise use the main list.
-	 */
-	Person *person = nil;
-	if (tableView == self.searchDisplayController.searchResultsTableView)
-	{
-        person = [self.filteredListContent objectAtIndex:indexPath.row];
-    }
-	else
-	{
-        person = [self.listContent objectAtIndex:indexPath.row];
-    }
-	detailsViewController.title = person.firstName;
-    
-    [[self navigationController] pushViewController:detailsViewController animated:YES];
-    [detailsViewController release];
+	NSLog(@">>> Entering %s <<<", __PRETTY_FUNCTION__);
+	
+	[tableView deselectRowAtIndexPath:indexPath animated:YES];
+	
+	NSLog(@"<<< Leaving %s >>>", __PRETTY_FUNCTION__);
 }
 
+#pragma mark -
+#pragma mark UISearchDisplayController Delegate Methods
 
-
-- (void)filterContentForSearchText:(NSString*)searchText scope:(NSString*)scope
+- (BOOL)searchDisplayController:(UISearchDisplayController *)controller 
+shouldReloadTableForSearchString:(NSString *)searchString
 {
-	/*
-	 Update the filtered array based on the search text and scope.
-	 */
+	NSLog(@">>> Entering %s <<<", __PRETTY_FUNCTION__);
 	
-	[self.filteredListContent removeAllObjects]; // First clear the filtered array.
-	
-	/*
-	 Search the main list for products whose type matches the scope (if selected) and whose name matches searchText; add items that match to the filtered array.
-	 */
-	for (Person *person in listContent)
-	{
-		if ([scope isEqualToString:@"All"] || [person.type isEqualToString:scope])
-		{
-			NSComparisonResult result = [person.firstName compare:searchText options:(NSCaseInsensitiveSearch|NSDiacriticInsensitiveSearch) range:NSMakeRange(0, [searchText length])];
-            if (result == NSOrderedSame)
-			{
-				[self.filteredListContent addObject:person];
-            }
-		}
-	}
-}
-
-
-
-- (BOOL)searchDisplayController:(UISearchDisplayController *)controller shouldReloadTableForSearchString:(NSString *)searchString
-{
-    [self filterContentForSearchText:searchString scope:
-     [[self.searchDisplayController.searchBar scopeButtonTitles] objectAtIndex:[self.searchDisplayController.searchBar selectedScopeButtonIndex]]];
+	[self handleSearchForTerm:searchString];
     
     // Return YES to cause the search result table view to be reloaded.
+	NSLog(@"<<< Leaving %s >>>", __PRETTY_FUNCTION__);
     return YES;
 }
 
-
-- (BOOL)searchDisplayController:(UISearchDisplayController *)controller shouldReloadTableForSearchScope:(NSInteger)searchOption
+- (void)searchDisplayControllerWillEndSearch:(UISearchDisplayController *)controller
 {
-    [self filterContentForSearchText:[self.searchDisplayController.searchBar text] scope:
-     [[self.searchDisplayController.searchBar scopeButtonTitles] objectAtIndex:searchOption]];
-    
-    // Return YES to cause the search result table view to be reloaded.
-    return YES;
+	NSLog(@">>> Entering %s <<<", __PRETTY_FUNCTION__);
+	
+	[self setSavedSearchTerm:nil];
+	
+	[[self mainTableView] reloadData];
+	
+	NSLog(@"<<< Leaving %s >>>", __PRETTY_FUNCTION__);
 }
-
 
 @end
