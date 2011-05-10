db55639e693b1d4ba323fee1468fbd8d8cb621db
Rking188@gmail.com <Rking188@gmail.com@ce22a5dd-5176-1b88-f200-a682eaf39866>
2011-05-10 18:53:51 +0000

    Added an academic calendar operation to the main operation page. TODO: Need to change the icon for the academic calendar button.

--- UMaine/src/org/umece/android/umaine/UMaine.java
+++ UMaine/src/org/umece/android/umaine/UMaine.java
@@ -5,12 +5,17 @@ import org.umece.android.umaine.sports.UMSports;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 public class UMaine extends Activity {
-    /** Called when the activity is first created. */
+	/** TODO: Possibly add a button to the main screen to link to the 
+	 * umaine website for the acadmic calendar
+	 */
+	
+	/** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -51,9 +56,19 @@ public class UMaine extends Activity {
         Button dirbtn = (Button) findViewById(R.id.directorybtn);
         dirbtn.setOnClickListener(new View.OnClickListener() {
 			
-			public void onClick(View v) {
+			public void onClick(View v) { 
 				Intent myIntent = new Intent(v.getContext(), UMDirectory.class);
 				startActivityForResult(myIntent, 0);
+			}
+		});
+        
+        /* Set up the listener for the Calendar button */
+        Button calbtn = (Button) findViewById(R.id.calendarbtn);
+        calbtn.setOnClickListener(new View.OnClickListener() {
+			
+			public void onClick(View v) {
+				Intent viewIntent = new Intent("android.intent.action.VIEW", Uri.parse("http://calendar.umaine.edu/academic-calendar/"));
+				startActivity(viewIntent);
 			}
 		});
     }
